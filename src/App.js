e621c006d0a9414a54e0180b30123a5ebcd58da3
knjko <me@knjko.org>
2017-04-15 16:18:57 +0900

    Add react-tap-event-plugin

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Tracklistify from './Tracklistify';
 import './App.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 class App extends Component {
   render() {
